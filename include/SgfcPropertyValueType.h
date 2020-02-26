5e8948a798fb1e58087a2186643d08af9f07f2c0
Patrick Näf <herzbube@herzbube.ch>
2020-02-26 23:53:27 +0100

    improve docs for enum SgfcPropertyValueType

--- include/SgfcPropertyValueType.h
+++ include/SgfcPropertyValueType.h
@@ -2,36 +2,35 @@
 
 namespace LibSgfcPlusPlus
 {
-  /// @brief SgfcPropertyValueType enumerates all SGF property value types.
+  /// @brief SgfcPropertyValueType enumerates the basic SGF property value
+  /// types.
   enum class SgfcPropertyValueType
   {
     /// @brief The property has no value.
-    ///
-    /// @todo Do we really need this?
     None,
-    /// @brief A positive or negative integer value.
+    /// @brief The property value is a positive or negative integer.
     Number,
-    /// @brief A positive or negative decimal value.
+    /// @brief The property value is a positive or negative decimal value.
     Real,
-    /// @brief A Double value can have one of two values: Either "1" (which
-    /// means "normal") or "2" (which means "emphasized").
+    /// @brief The property value is a Double value. A Double value can have
+    /// one of two integer values: Either 1 (which means "normal") or 2 (which
+    /// means "emphasized").
     ///
     /// Example: GB[1] means "good for black" (normal). GB[2] means "very good
     /// for black" (emphasized).
     Double,
-    /// @brief A Color value can have one of two values: Either "B" (which
-    /// means "black") or "W" (which means "white").
+    /// @brief The property value is a Color value. A Color value can have one
+    /// of two string values: Either "B" (which means "black") or "W" (which
+    /// means "white").
     Color,
     SimpleText,
     Text,
     Point,
     Move,
     Stone,
-
-    /// @brief The property value has a custom type. This is used for all
-    /// values of a property that is not defined in the SGF standard.
-    Custom,
-    /// @brief The type of the property value is unknown.
+    /// @brief The property value has an unknown type. The property could even
+    /// have no value at all. This is used for the values of any property not
+    /// defined in the SGF standard.
     Unknown,
   };
 }
