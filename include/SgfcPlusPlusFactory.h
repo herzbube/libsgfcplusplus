ad4f385ff8d63d67b85f4acd9a57807ca7be61c6
Patrick Näf <herzbube@herzbube.ch>
2020-03-11 19:53:35 +0100

    SgfcPlusPlusFactory can now create many more objects; still missing: property values

--- include/SgfcPlusPlusFactory.h
+++ include/SgfcPlusPlusFactory.h
@@ -42,6 +42,82 @@ namespace LibSgfcPlusPlus
     /// @brief Returns a newly constructed ISgfcDocumentWriter object.
     static std::shared_ptr<ISgfcDocumentWriter> CreateDocumentWriter();
 
+    /// @brief Returns a newly constructed ISgfcDocument object. The
+    /// ISgfcDocument is empty and contains no games.
+    static std::shared_ptr<ISgfcDocument> CreateDocument();
+
+    /// @brief Returns a newly constructed ISgfcDocument object. The
+    /// ISgfcDocument content consists of the specified game @a game. The
+    /// ISgfcDocument object takes ownership of @a game.
+    static std::shared_ptr<ISgfcDocument> CreateDocument(std::shared_ptr<ISgfcGame> game);
+
+    /// @brief Returns a newly constructed ISgfcGame object. The game type is
+    /// SgfcGameType::Unknown. The game is not associated with any document.
+    /// The game has no game tree.
+    static std::shared_ptr<ISgfcGame> CreateGame();
+
+    /// @brief Returns a newly constructed ISgfcGame object. The game tree's
+    /// root node is @a rootNode. The game type depends on the content of the
+    /// root node. The game is not associated with any document. The ISgfcGame
+    /// object takes ownership of @a rootNode.
+    static std::shared_ptr<ISgfcGame> CreateGame(std::shared_ptr<ISgfcNode> rootNode);
+
+    /// @brief Returns a newly constructed ISgfcNode object that has no parent,
+    /// child or sibling and is not associated with any game.
+    static std::shared_ptr<ISgfcNode> CreateNode();
+
+    /// @brief Returns a newly constructed ISgfcNode object that is a child node
+    /// of @a parentNode. The node is appended as the last child to @a parent.
+    static std::shared_ptr<ISgfcNode> CreateChildNode(std::shared_ptr<ISgfcNode> parentNode);
+
+    /// @brief Returns a newly constructed ISgfcProperty object that has the
+    /// specified property type @a propertyType. The property has no values.
+    /// The property is not associated with any node.
+    ///
+    /// @exception std::invalid_argument Is thrown if @a propertyType is
+    /// SgfcPropertyType::Unknown.
+    static std::shared_ptr<ISgfcProperty> CreateProperty(SgfcPropertyType propertyType);
+
+    /// @brief Returns a newly constructed ISgfcProperty object that has the
+    /// specified property type @a propertyType and the specified property
+    /// values @a propertyValues. The property is not associated with any node.
+    /// The ISgfcProperty object takes ownership of the ISgfcPropertyValue
+    /// objects in @a propertyValues.
+    ///
+    /// @exception std::invalid_argument Is thrown if @a propertyType is
+    /// SgfcPropertyType::Unknown.
+    static std::shared_ptr<ISgfcProperty> CreateProperty(
+      SgfcPropertyType propertyType,
+      const std::vector<std::shared_ptr<ISgfcPropertyValue>>& propertyValues);
+
+    /// @brief Returns a newly constructed ISgfcProperty object that has the
+    /// specified property name @a propertyName. The property type is inferred
+    /// from the property name. The property has no values. The property is not
+    /// associated with any node.
+    ///
+    /// Attempts to map @a propertyName to a value in the enumeration
+    /// SgfcPropertyType. If the mapping succeeds the result of invoking this
+    /// method is the same as invoking the overload that takes an
+    /// SgfcPropertyType value. If the mapping fails then the resulting
+    /// ISgfcProperty object has SgfcPropertyType::Unknown.
+    static std::shared_ptr<ISgfcProperty> CreateProperty(const std::string& propertyName);
+
+    /// @brief Returns a newly constructed ISgfcProperty object that has the
+    /// specified property name @a propertyName and the specified property
+    /// values @a propertyValues. The property type is inferred from the
+    /// property name. The property is not associated with any node.
+    /// The ISgfcProperty object takes ownership of the ISgfcPropertyValue
+    /// objects in @a propertyValues.
+    ///
+    /// Attempts to map @a propertyName to a value in the enumeration
+    /// SgfcPropertyType. If the mapping succeeds the result of invoking this
+    /// method is the same as invoking the overload that takes an
+    /// SgfcPropertyType value. If the mapping fails then the resulting
+    /// ISgfcProperty object has SgfcPropertyType::Unknown.
+    static std::shared_ptr<ISgfcProperty> CreateProperty(
+      const std::string& propertyName,
+      const std::vector<std::shared_ptr<ISgfcPropertyValue>>& propertyValues);
+
   private:
     SgfcPlusPlusFactory();
     ~SgfcPlusPlusFactory();
--- src/SgfcPlusPlusFactory.cpp
+++ src/SgfcPlusPlusFactory.cpp
@@ -1,8 +1,19 @@
 // Project includes
+#include "../include/SgfcConstants.h"
 #include "../include/SgfcPlusPlusFactory.h"
+#include "document/SgfcDocument.h"
+#include "document/SgfcGame.h"
+#include "document/SgfcNode.h"
+#include "document/SgfcProperty.h"
+#include "document/SgfcTreeBuilder.h"
+#include "parsing/SgfcPropertyDecoder.h"
 #include "sgfc/frontend/SgfcCommandLine.h"
 #include "sgfc/frontend/SgfcDocumentReader.h"
 #include "sgfc/frontend/SgfcDocumentWriter.h"
+
+// C++ Standard Library includes
+#include <sstream>
+#include <stdexcept>
 
 namespace LibSgfcPlusPlus
 {
@@ -25,10 +36,103 @@ namespace LibSgfcPlusPlus
     return reader;
   }
 
-  /// @brief Returns a newly constructed ISgfcDocumentWriter object.
   std::shared_ptr<ISgfcDocumentWriter> SgfcPlusPlusFactory::CreateDocumentWriter()
   {
     std::shared_ptr<ISgfcDocumentWriter> writer = std::shared_ptr<ISgfcDocumentWriter>(new SgfcDocumentWriter());
     return writer;
+  }
+
+  std::shared_ptr<ISgfcDocument> SgfcPlusPlusFactory::CreateDocument()
+  {
+    std::shared_ptr<ISgfcDocument> document = std::shared_ptr<ISgfcDocument>(new SgfcDocument());
+    return document;
+  }
+
+  std::shared_ptr<ISgfcDocument> SgfcPlusPlusFactory::CreateDocument(std::shared_ptr<ISgfcGame> game)
+  {
+    std::shared_ptr<ISgfcDocument> document = CreateDocument();
+    document->AppendGame(game);
+    return document;
+  }
+
+  std::shared_ptr<ISgfcGame> SgfcPlusPlusFactory::CreateGame()
+  {
+    return CreateGame(nullptr);
+  }
+
+  std::shared_ptr<ISgfcGame> SgfcPlusPlusFactory::CreateGame(std::shared_ptr<ISgfcNode> rootNode)
+  {
+    std::shared_ptr<SgfcGame> game = std::shared_ptr<SgfcGame>(new SgfcGame(rootNode));
+
+    std::shared_ptr<ISgfcTreeBuilder> treeBuilder =
+      std::shared_ptr<ISgfcTreeBuilder>(new SgfcTreeBuilder(game));
+    game->SetTreeBuilder(treeBuilder);
+
+    return game;
+  }
+
+  std::shared_ptr<ISgfcNode> SgfcPlusPlusFactory::CreateNode()
+  {
+    std::shared_ptr<ISgfcNode> node = std::shared_ptr<ISgfcNode>(new SgfcNode());
+    return node;
+  }
+
+  std::shared_ptr<ISgfcNode> SgfcPlusPlusFactory::CreateChildNode(std::shared_ptr<ISgfcNode> parentNode)
+  {
+    std::shared_ptr<ISgfcNode> node = std::shared_ptr<ISgfcNode>(new SgfcNode());
+    return node;
+  }
+
+  std::shared_ptr<ISgfcProperty> SgfcPlusPlusFactory::CreateProperty(SgfcPropertyType propertyType)
+  {
+    auto it = SgfcConstants::PropertyTypeToPropertyNameMap.find(propertyType);
+
+    if (it == SgfcConstants::PropertyTypeToPropertyNameMap.cend())
+    {
+      std::stringstream message;
+      message << "Property type argument has unsupported value: " << static_cast<int>(propertyType);
+      throw std::invalid_argument(message.str());
+    }
+
+    std::string propertyName = it->second;
+    std::shared_ptr<ISgfcProperty> property = std::shared_ptr<ISgfcProperty>(new SgfcProperty(
+      propertyType,
+      propertyName));
+
+    return property;
+  }
+
+  std::shared_ptr<ISgfcProperty> SgfcPlusPlusFactory::CreateProperty(
+    SgfcPropertyType propertyType,
+    const std::vector<std::shared_ptr<ISgfcPropertyValue>>& propertyValues)
+  {
+    std::shared_ptr<ISgfcProperty> property = CreateProperty(propertyType);
+
+    property->SetPropertyValues(propertyValues);
+
+    return property;
+  }
+
+  std::shared_ptr<ISgfcProperty> SgfcPlusPlusFactory::CreateProperty(const std::string& propertyName)
+  {
+    SgfcPropertyType propertyType =
+      SgfcPropertyDecoder::MapPropertyNameToPropertyType(propertyName);
+
+    std::shared_ptr<ISgfcProperty> property = std::shared_ptr<ISgfcProperty>(new SgfcProperty(
+      propertyType,
+      propertyName));
+
+    return property;
+  }
+
+  std::shared_ptr<ISgfcProperty> SgfcPlusPlusFactory::CreateProperty(
+    const std::string& propertyName,
+    const std::vector<std::shared_ptr<ISgfcPropertyValue>>& propertyValues)
+  {
+    std::shared_ptr<ISgfcProperty> property = CreateProperty(propertyName);
+
+    property->SetPropertyValues(propertyValues);
+
+    return property;
   }
 }
