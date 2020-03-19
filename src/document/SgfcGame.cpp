79a161901a576eb0e9ad55c1dc80edfd1006d343
Patrick Näf <herzbube@herzbube.ch>
2020-03-19 01:06:46 +0100

    simplify SgfcGame::GetBoardSize by making use of ISgfcBoardSizeProperty

--- src/document/SgfcGame.cpp
+++ src/document/SgfcGame.cpp
@@ -1,4 +1,5 @@
 // Project includes
+#include "../../include/ISgfcBoardSizeProperty.h"
 #include "../../include/ISgfcComposedPropertyValue.h"
 #include "../../include/ISgfcNumberPropertyValue.h"
 #include "../../include/ISgfcProperty.h"
@@ -80,59 +81,7 @@ namespace LibSgfcPlusPlus
     if (property == nullptr)
       return SgfcConstants::BoardSizeNone;
 
-    // We are interested only in the first value because the property is
-    // supposed to have only one value.
-    auto propertyValue = property->GetPropertyValue();
-    if (propertyValue == nullptr)
-    {
-      switch (GetGameType())
-      {
-        case SgfcGameType::Go:
-          return SgfcConstants::BoardSizeDefaultGo;
-        case SgfcGameType::Chess:
-          return SgfcConstants::BoardSizeDefaultChess;
-        default:
-          return SgfcConstants::BoardSizeNone;
-      }
-    }
-
-    if (propertyValue->IsComposedValue())
-    {
-      const auto* composedValue = propertyValue->ToComposedValue();
-
-      SgfcNumber numberOfColumns = -1;
-      SgfcNumber numberOfRows = -1;
-
-      auto singleValue1 = composedValue->GetValue1();
-      if (singleValue1->HasTypedValue())
-      {
-        if (singleValue1->GetValueType() == SgfcPropertyValueType::Number)
-          numberOfColumns = singleValue1->ToNumberValue()->GetNumberValue();
-      }
-      auto singleValue2 = composedValue->GetValue2();
-      if (singleValue2->HasTypedValue())
-      {
-        if (singleValue2->GetValueType() == SgfcPropertyValueType::Number)
-          numberOfRows = singleValue2->ToNumberValue()->GetNumberValue();
-      }
-
-      if (numberOfColumns != -1 && numberOfRows != -1)
-        return { numberOfColumns, numberOfRows };
-    }
-    else
-    {
-      const auto* singleValue = propertyValue->ToSingleValue();
-      if (singleValue->HasTypedValue())
-      {
-        if (singleValue->GetValueType() == SgfcPropertyValueType::Number)
-        {
-          SgfcNumber numberOfColumnsAndRows = singleValue->ToNumberValue()->GetNumberValue();
-          return { numberOfColumnsAndRows, numberOfColumnsAndRows };
-        }
-      }
-    }
-
-    return SgfcConstants::BoardSizeNone;
+    return property->ToBoardSizeProperty()->GetBoardSize(this->GetGameType());
   }
 
   bool SgfcGame::HasRootNode() const
