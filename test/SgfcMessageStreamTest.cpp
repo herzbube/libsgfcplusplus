4abc151fc6a8d2927f361a8c453e4f9614d2e240
Patrick Näf <herzbube@herzbube.ch>
2020-06-04 00:28:07 +0200

    SgfcMessageStreamTest: fix nesting of GIVEN blocks

--- test/SgfcMessageStreamTest.cpp
+++ test/SgfcMessageStreamTest.cpp
@@ -5,7 +5,9 @@
 // Unit test library includes
 #include <catch2/catch.hpp>
 
-// SGFC includes to be able to invoke the SGFC function PrintError()
+// SGFC includes to be able to invoke the SGFC function PrintError() and
+// the function WriteFormattedStringToLibSgfcPlusPlusMessageStream() patched
+// into SGFC by libsgfc++.
 extern "C"
 {
   #include <../sgfc/all.h>
@@ -64,109 +66,109 @@ SCENARIO( "SgfcMessageStream acquires message stream content from SGFC", "[sgfc-
         REQUIRE( rawMessageStreamContent == "foobar" );
       }
     }
+  }
 
-    GIVEN( "The message stream contains two lines using LF as line delimiter" )
+  GIVEN( "The message stream contains two lines using LF as line delimiter" )
+  {
+    ResetSgfcMessageStream();
+
+    WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "foo");
+    WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "\n");
+    WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "bar");
+
+    WHEN( "SgfcMessageStream is constructed" )
     {
-      ResetSgfcMessageStream();
+      SgfcMessageStream messageStream;
 
-      WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "foo");
-      WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "\n");
-      WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "bar");
-
-      WHEN( "SgfcMessageStream is constructed" )
+      THEN( "The SgfcMessageStream object has two lines of content" )
       {
-        SgfcMessageStream messageStream;
+        auto messageStreamLines = messageStream.GetMessageStreamLines();
+        auto rawMessageStreamContent = messageStream.GetRawMessageStreamContent();
 
-        THEN( "The SgfcMessageStream object has two lines of content" )
-        {
-          auto messageStreamLines = messageStream.GetMessageStreamLines();
-          auto rawMessageStreamContent = messageStream.GetRawMessageStreamContent();
+        REQUIRE( messageStreamLines.size() == 2 );
+        REQUIRE( messageStreamLines.front() == "foo" );
+        REQUIRE( messageStreamLines.back() == "bar" );
+        REQUIRE( rawMessageStreamContent == "foo\nbar" );
+      }
+    }
+  }
 
-          REQUIRE( messageStreamLines.size() == 2 );
-          REQUIRE( messageStreamLines.front() == "foo" );
-          REQUIRE( messageStreamLines.back() == "bar" );
-          REQUIRE( rawMessageStreamContent == "foo\nbar" );
-        }
+  // The difference to the previous test is that we let PrintError decide
+  // what it wants to use as a line delimiter. This ***SHOULD*** be LF, but
+  // a future implementation of SGFC might change this to something else.
+  // If that happens SgfcMessageStream will break, and by testing it we will
+  // notice.
+  GIVEN( "The message stream contains two lines using the PrintError-internal line delimiter" )
+  {
+    ResetSgfcMessageStream();
+
+    // Set up the buffer. For the message type that we want PrintError()
+    // needs to scans the buffer to determine the line/column number.
+    sgfc = new SGFInfo();
+    sgfc->buffer = const_cast<char*>("");  // cast is safe, nobody is going to modify the buffer
+    sgfc->start = sgfc->buffer;
+    sgfc->b_end = sgfc->start + 1;
+
+    // Without this PrintError() does not output any messages
+    memset(error_enabled, TRUE, sizeof(error_enabled));
+
+    PrintError(E_UNEXPECTED_EOF, sgfc->start);
+    PrintError(E_UNEXPECTED_EOF, sgfc->start);
+
+    WHEN( "SgfcMessageStream is constructed" )
+    {
+      SgfcMessageStream messageStream;
+
+      THEN( "The SgfcMessageStream object has two lines of content" )
+      {
+        std::string expectedMessageLine = "Line:1 Col:1 - Error 10 (critical): unexpected end of file";
+        std::string expectedRawMessageStreamContent =
+          expectedMessageLine +
+          SgfcPrivateConstants::NewlineCharacter +
+          expectedMessageLine +
+          SgfcPrivateConstants::NewlineCharacter;
+
+        auto messageStreamLines = messageStream.GetMessageStreamLines();
+        auto rawMessageStreamContent = messageStream.GetRawMessageStreamContent();
+
+        REQUIRE( messageStreamLines.size() == 2 );
+        // The CR character is not recognized by SgfcMessageStream as
+        // belonging to the line delimiter. This is an implementation detail
+        // that we test here in case
+        REQUIRE( messageStreamLines.front() == expectedMessageLine );
+        REQUIRE( messageStreamLines.back() == expectedMessageLine );
+        REQUIRE( rawMessageStreamContent ==  expectedRawMessageStreamContent);
       }
     }
 
-    // The difference to the previous test is that we let PrintError decide
-    // what it wants to use as a line delimiter. This ***SHOULD*** be LF, but
-    // a future implementation of SGFC might change this to something else.
-    // If that happens SgfcMessageStream will break, and by testing it we will
-    // notice.
-    GIVEN( "The message stream contains two lines using the PrintError-internal line delimiter" )
+    delete sgfc;
+  }
+
+  GIVEN( "Two SgfcMessageStream object are constructed" )
+  {
+    ResetSgfcMessageStream();
+
+    WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "foo");
+    SgfcMessageStream messageStream1;
+
+    WHEN( "The second SgfcMessageStream object is constructed" )
     {
-      ResetSgfcMessageStream();
+      SgfcMessageStream messageStream2;
 
-      // Set up the buffer. For the message type that we want PrintError()
-      // needs to scans the buffer to determine the line/column number.
-      sgfc = new SGFInfo();
-      sgfc->buffer = const_cast<char*>("");  // cast is safe, nobody is going to modify the buffer
-      sgfc->start = sgfc->buffer;
-      sgfc->b_end = sgfc->start + 1;
-
-      // Without this PrintError() does not output any messages
-      memset(error_enabled, TRUE, sizeof(error_enabled));
-
-      PrintError(E_UNEXPECTED_EOF, sgfc->start);
-      PrintError(E_UNEXPECTED_EOF, sgfc->start);
-
-      WHEN( "SgfcMessageStream is constructed" )
+      THEN( "The second SgfcMessageStream object has no content" )
       {
-        SgfcMessageStream messageStream;
+        auto messageStream1Lines = messageStream1.GetMessageStreamLines();
+        auto rawMessageStream1Content = messageStream1.GetRawMessageStreamContent();
 
-        THEN( "The SgfcMessageStream object has two lines of content" )
-        {
-          std::string expectedMessageLine = "Line:1 Col:1 - Error 10 (critical): unexpected end of file";
-          std::string expectedRawMessageStreamContent =
-            expectedMessageLine +
-            SgfcPrivateConstants::NewlineCharacter +
-            expectedMessageLine +
-            SgfcPrivateConstants::NewlineCharacter;
+        REQUIRE( messageStream1Lines.size() == 1 );
+        REQUIRE( messageStream1Lines.front() == "foo" );
+        REQUIRE( rawMessageStream1Content == "foo" );
 
-          auto messageStreamLines = messageStream.GetMessageStreamLines();
-          auto rawMessageStreamContent = messageStream.GetRawMessageStreamContent();
+        auto messageStream2Lines = messageStream2.GetMessageStreamLines();
+        auto rawMessageStream2Content = messageStream2.GetRawMessageStreamContent();
 
-          REQUIRE( messageStreamLines.size() == 2 );
-          // The CR character is not recognized by SgfcMessageStream as
-          // belonging to the line delimiter. This is an implementation detail
-          // that we test here in case
-          REQUIRE( messageStreamLines.front() == expectedMessageLine );
-          REQUIRE( messageStreamLines.back() == expectedMessageLine );
-          REQUIRE( rawMessageStreamContent ==  expectedRawMessageStreamContent);
-        }
-      }
-
-      delete sgfc;
-    }
-
-    GIVEN( "Two SgfcMessageStream object are constructed" )
-    {
-      ResetSgfcMessageStream();
-
-      WriteFormattedStringToLibSgfcPlusPlusMessageStream("%s", "foo");
-      SgfcMessageStream messageStream1;
-
-      WHEN( "The second SgfcMessageStream object is constructed" )
-      {
-        SgfcMessageStream messageStream2;
-
-        THEN( "The second SgfcMessageStream object has no content" )
-        {
-          auto messageStream1Lines = messageStream1.GetMessageStreamLines();
-          auto rawMessageStream1Content = messageStream1.GetRawMessageStreamContent();
-
-          REQUIRE( messageStream1Lines.size() == 1 );
-          REQUIRE( messageStream1Lines.front() == "foo" );
-          REQUIRE( rawMessageStream1Content == "foo" );
-
-          auto messageStream2Lines = messageStream2.GetMessageStreamLines();
-          auto rawMessageStream2Content = messageStream2.GetRawMessageStreamContent();
-
-          REQUIRE( messageStream2Lines.size() == 0 );
-          REQUIRE( rawMessageStream2Content == "" );
-        }
+        REQUIRE( messageStream2Lines.size() == 0 );
+        REQUIRE( rawMessageStream2Content == "" );
       }
     }
   }
