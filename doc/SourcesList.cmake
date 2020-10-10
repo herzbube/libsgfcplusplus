483bf8d0ee165011b3885aa2e0b0ae3a1f120fe0
Patrick Näf <herzbube@herzbube.ch>
2020-10-10 20:46:57 +0200

    Doxygen documentation is now generated via CMake (#13)

---
+++ doc/SourcesList.cmake
@@ -0,0 +1,14 @@
+set (
+  SOURCES
+  ../include
+  ../src
+  ../doc
+  ../README.md
+)
+
+set (
+  OUTPUT_DIRECTORIES
+  ${CMAKE_CURRENT_BINARY_DIR}/html
+  # Add more directories here if you have configured Doxygen to generate more
+  # output.
+)
