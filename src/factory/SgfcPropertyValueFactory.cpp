b8ecf68813e24e4c72568fc4fc1073f9437ede49
Patrick Näf <herzbube@herzbube.ch>
2026-01-20 21:42:10 +0100

    fix SgfcNumber-to-string conversion (#47)

--- src/factory/SgfcPropertyValueFactory.cpp
+++ src/factory/SgfcPropertyValueFactory.cpp
@@ -54,6 +54,11 @@ namespace LibSgfcPlusPlus
     SgfcNumber numberValue) const
   {
     std::stringstream stream;
+
+    // Make sure that decimal point is always a period (".") character and that
+    // there are no thousands separators
+    stream.imbue(std::locale::classic());
+
     stream << numberValue;
 
     std::shared_ptr<ISgfcNumberPropertyValue> valueObject = std::shared_ptr<ISgfcNumberPropertyValue>(
